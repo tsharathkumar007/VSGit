00dc8d9b6f852ebeef367d2cfbac9781d71a49f1
SHARAT KUMAR <sharat.kumar.ext@siemens-healthineers.com>
2020-02-24 16:04:21 +0530

    ss

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp2
            //Update from GITHUB Server
            //Update from VS IDE
            //Created New Branch Feature from VS
-           //Update from Github Feature Branch
+           //Update from Github Feature Branch'
+           //Sharath
         }
     }
 }
